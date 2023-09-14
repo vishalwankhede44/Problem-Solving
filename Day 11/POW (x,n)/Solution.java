fc0cc30c49d12ce79a9ace5e6c29e8a4648d3129
Vishal <vishalwankhede44@gmail.com>
2023-09-14 22:34:16 +0530

    Day 11 code push

---
+++ Day 11/POW (x,n)/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long nn = n;
+        double ans=1.0;
+
+        if(n<0)
+            nn = nn * -1;
+        
+        while(nn>0){
+            if(nn%2==1){
+                ans = ans * x;
+                nn--;
+            }else{
+                x = x*x;
+                nn = nn/2;
+            }
+        }
+
+        if(n<0)
+            ans = (double)1/(double)ans;
+
+        return ans;
+
+    }
+}
